5ff0e45a7443ad1cc3abcf2e2a01153c6844ff53
DANCPTS <daniel@cpts.uk>
2025-12-09 12:08:33 +0000

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,9 @@ export default function RootLayout({
       <body className={montserrat.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem={true}
+          defaultTheme="light"
+          enableSystem={false}
+          themes={['light', 'dark', 'ocean', 'forest']}
           storageKey="cpts-theme"
           disableTransitionOnChange
         >
